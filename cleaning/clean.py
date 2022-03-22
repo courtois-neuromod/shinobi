ca2a8d504df84e86096533822b63bd403a61ad82
Hyruuk <yharel109@gmail.com>
2022-03-22 19:58:21 -0400

    now cleans Unnamed column names and doesnt save index anymore

--- cleaning/clean.py
+++ cleaning/clean.py
@@ -3,27 +3,34 @@ import os.path as op
 import pandas as pd
 import numpy as np
 
+
 def clean_eventfiles():
-    datapath = './'
+    datapath = "./"
     eventfiles_list = []
     for root, directory, files in os.walk(datapath):
         for file in files:
-            if 'events.tsv' in file:
+            if "events.tsv" in file:
                 eventfiles_list.append(op.join(root, file))
 
     for eventfile in sorted(eventfiles_list):
-        dataframe = pd.read_csv(eventfile, sep='\t')
-        dataframe = dataframe.fillna('Missing file')
-        for idx, stimfile in enumerate(dataframe['stim_file']):
-            if '/' in stimfile and not 'behavior' in stimfile:
-                split_string = stimfile.split('/')
-                split_string.insert(1, 'behavior')
-                final_string = '/'.join(split_string)
-                dataframe['stim_file'][idx] = final_string
-        dataframe.to_csv(eventfile, sep='\t', index=False)
+        dataframe = pd.read_csv(eventfile, sep="\t")
+        dataframe = dataframe.fillna("Missing file")
+        # remove junk columns
+        for column_name in dataframe.columns:
+            if "Unnamed" in column_name:
+                dataframe = dataframe.drop(columns=column_name)
+        for idx, stimfile in enumerate(dataframe["stim_file"]):
+            if "/" in stimfile and not "behavior" in stimfile:
+                split_string = stimfile.split("/")
+                split_string.insert(1, "behavior")
+                final_string = "/".join(split_string)
+                dataframe["stim_file"][idx] = final_string
+        dataframe.to_csv(eventfile, sep="\t", index=False)
+
 
 def main():
     clean_eventfiles()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
