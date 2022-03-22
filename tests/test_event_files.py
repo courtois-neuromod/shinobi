ee6b96aab71e9d82e877311b2775a35f0c575cfe
François Paugam <35327799+FrancoisPgm@users.noreply.github.com>
2022-03-22 18:21:31 -0400

    print lists in test_events

--- tests/test_event_files.py
+++ tests/test_event_files.py
@@ -6,6 +6,10 @@ import csv
 import json
 
 
+BK2_DURATION_DIFF_THRES = 0.2
+BOLD_DURATION_DIFF_THRES = 30
+
+
 def test_eventfiles():
     """Checks that all the .bk2 files mentioned in the events files are present
     in the sourcedata folder.
@@ -22,40 +26,41 @@ def test_eventfiles():
     for eventfile in sorted(eventfiles_list):
         event_dataframe = pd.read_csv(eventfile, sep="\t")
         for filepath in event_dataframe["stim_file"]:
-            if type(filepath) == str:
+            if filepath != "Missing file":
                 bk2files_fromevents.append(op.join(datapath, filepath))
 
     bk2files_infolder = []
     for root, directory, files in os.walk(datapath):
         for file in files:
             if ".bk2" in file and "ShinobiIII" in file:
+                print(op.join(root, file))
                 bk2files_infolder.append(op.join(root, file))
 
-    # TODO : add './' in front of bk2files_fromevents
     bk2files_infolder.sort()
     bk2files_fromevents.sort()
 
-    in_source_in_events = [x for x in bk2files_infolder if x in bk2files_fromevents]
+    # Get a list of bk2 files that are referenced in the events.tsv but not found in sourcedata
+    in_events_not_in_source = [x for x in bk2files_fromevents if x not in bk2files_infolder]
 
-    in_events_not_in_source = [
-        x for x in bk2files_fromevents if x not in bk2files_infolder
-    ]
+    # Get a list of bk2 files that aren't referenced in the events.tsv
+    in_source_not_in_events = [x for x in bk2files_infolder if x not in bk2files_fromevents]
 
-    # Get a list of bk2 files that aren't reference in the events.tsv
-    in_source_not_in_events = [
-        x for x in bk2files_infolder if x not in bk2files_fromevents
-    ]
-    with open("bk2files_not_in_events.log", "w") as f:
-        f.write("\n".join(in_source_not_in_events))
+    error_msg = ""
+    if in_events_not_in_source:
+        error_msg += (
+            "\nFollowing files referenced in event files are not in the sourcedata folder:\n"
+        )
+        error_msg += "\n".join(in_events_not_in_source)
 
-    assert (
-        len(in_events_not_in_source) == 0
-    ), "Files referenced in the events.tsv are missing from the sourcedata folder."
-    # TODO : print la liste des fichiers manquants
-    # TODO : ajouter ces fichiers dans un fichier créé pour l'occasion et le supprimer si le test réussi
+    if in_source_not_in_events:
+        error_msg += "\nFollowing in the sourcedata folder are not referenced in event files:\n"
+        error_msg += "\n".join(in_source_not_in_events)
+
+    assert not in_events_not_in_source and not in_source_not_in_events, error_msg
 
 
 def test_event_files_not_empty():
+    """Test event files don't contain just the header."""
     empty_files = []
     for event_path in glob.glob("sub-*/ses-*/func/*_events.tsv"):
         with open(event_path, "r") as f:
@@ -64,10 +69,12 @@ def test_event_files_not_empty():
         if n_lines < 2:
             empty_files.append(event_path)
 
-    assert not empty_files, "Empty event files :" + "\n".join(empty_files)
+    assert not empty_files, "\nEmpty event files :\n" + "\n".join(empty_files)
 
 
 def test_durations():
+    """Check duration and bk2_duration in event file match, and check duration in event file
+    corresponds to the duration mentioned in bold.json files."""
     problematic_bk2 = []
     problematic_bold = []
     for event_path in glob.glob("sub-*/ses-*/func/*_events.tsv"):
@@ -80,7 +87,7 @@ def test_durations():
                 onset, duration, duration_bk2, level, bk2_path = row[2:]
                 if (
                     bk2_path != "Missing file"
-                    and abs(float(duration) - float(duration_bk2)) > 0.1
+                    and abs(float(duration) - float(duration_bk2)) > BK2_DURATION_DIFF_THRES
                 ):
                     problematic_bk2.append(
                         f"{bk2_path} : duration={duration}, duration_bk2={duration_bk2}"
@@ -90,17 +97,21 @@ def test_durations():
 
             with open(json_path, "r") as f:
                 run_metadata = json.load(f)
-            bold_duration = float(
-                run_metadata["time"]["samples"]["AcquisitionTime"][-1]
-            ) - float(run_metadata["time"]["samples"]["AcquisitionTime"][0])
-            if bold_duration < run_duration or bold_duration > run_duration + 30:
+            bold_duration = run_metadata["dcmmeta_shape"][-1] * run_metadata["RepetitionTime"]
+            if (
+                bold_duration < run_duration
+                or bold_duration > run_duration + BOLD_DURATION_DIFF_THRES
+            ):
                 problematic_bold.append(
                     f"{json_path} : bold duration={bold_duration}, duration from events={run_duration}"
                 )
 
-    assert not problematic_bk2 and not problematic_bold, (
-        "Contradictory durations in bk2:\n"
-        + "\n".join(problematic_bk2)
-        + "\nContradictory durations in bold:\n"
-        + "\n".join(problematic_bold)
-    )
+    error_msg = ""
+    if problematic_bk2:
+        error_msg += "\nContradictory durations in bk2:\n"
+        error_msg += "\n".join(problematic_bk2)
+    if problematic_bold:
+        error_msg += "\nContradictory durations in bold:\n"
+        error_msg += "\n".join(problematic_bold)
+
+    assert not problematic_bk2 and not problematic_bold, error_msg
